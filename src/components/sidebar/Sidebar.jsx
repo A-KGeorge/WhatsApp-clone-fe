22f323e5bf0ba8d7380387bdb224cd46eacec468
A-KGeorge <alan.k.george11271@gmail.com>
2024-01-16 10:28:45 -0330

    Sidebar notifications component created

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,13 @@
 import { SidebarHeader } from "./header";
+import { Notifications } from "./notifications";
 
 export default function Sidebar() {
   return (
     <div className="w-[40%] h-full select-none">
       {/* Sidebar header */}
       <SidebarHeader />
+      {/* Notifications */}
+      <Notifications />
     </div>
   );
 }
