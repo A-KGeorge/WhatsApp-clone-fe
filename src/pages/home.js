6e1db311bc45251aaac5351045f087cd12bc865d
A-KGeorge <alan.k.george11271@gmail.com>
2024-01-17 13:36:59 -0330

    Chats Placeholder created

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,11 +2,12 @@ import { useEffect } from "react";
 import { Sidebar } from "../components/sidebar";
 import { useDispatch, useSelector } from "react-redux";
 import { getConversations } from "../features/chatSlice";
+import { WhatsappHome } from "../components/chat";
 
 export default function Home() {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
-
+  const { activeConversation } = useSelector((state) => state.chat);
   //get conversations
   useEffect(() => {
     if (user?.token) {
@@ -20,6 +21,7 @@ export default function Home() {
       <div className="container h-screen flex">
         {/* Sidebar */}
         <Sidebar />
+        {activeConversation ? "Home" : <WhatsappHome />}
       </div>
     </div>
   );
