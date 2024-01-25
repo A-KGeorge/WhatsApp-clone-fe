313f6d0dff947a9a280dc7d978f367e308cacf3a
A-KGeorge <alan.k.george11271@gmail.com>
2024-01-24 23:52:52 -0330

    socket io added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { io } from "socket.io-client";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+//pages
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+//socket io
+const socket = io(process.env.REACT_APP_API_ENDPOINT.split("/api/v1")[0]);
 
 function App() {
   const user = useSelector((state) => state.user);
