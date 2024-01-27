8795a7ecbc5f822a27f3bc986c3b47dd7200dd02
A-KGeorge <alan.k.george11271@gmail.com>
2024-01-26 21:31:56 -0330

    File preview input implemented

--- src/components/chat/preview/files/FilesPreview.jsx
+++ src/components/chat/preview/files/FilesPreview.jsx
@@ -2,8 +2,10 @@ import Header from "./Header";
 import FileViewer from "./FileViewer";
 import Input from "./Input";
 import HandleAndSend from "./HandleAndSend";
+import { useState } from "react";
 
 export default function FilesPreview() {
+  const [message, setMessage] = useState("");
   return (
     <div className="relative py-2 w-full flex items-center justify-center">
       {/* Container */}
@@ -12,9 +14,9 @@ export default function FilesPreview() {
         <Header />
         {/* Viewing selected file */}
         <FileViewer />
-        <div className="w-full flex flex-col">
+        <div className="w-full flex flex-col items-center">
           {/* Message input */}
-          <Input />
+          <Input message={message} setMessage={setMessage} />
           {/* Send and manipulate files */}
           <HandleAndSend />
         </div>
