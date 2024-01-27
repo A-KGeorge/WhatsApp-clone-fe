dd1372a25b0f3452d8d669d068c3c6817e5b28bf
A-KGeorge <alan.k.george11271@gmail.com>
2024-01-26 22:14:02 -0330

    adding other files functionality implemented

--- src/components/chat/preview/files/Add.jsx
+++ src/components/chat/preview/files/Add.jsx
@@ -1,3 +1,78 @@
+import { useRef } from "react";
+import { CloseIcon } from "../../../../svg";
+import { useDispatch } from "react-redux";
+import { addFiles } from "../../../../features/chatSlice";
+import { getFileType } from "../../../../utils/file";
+
 export default function Add({ setActiveIndex }) {
-  return <div>Add</div>;
+  const inputRef = useRef();
+  const dispatch = useDispatch();
+  const filesHandler = (e) => {
+    let files = Array.from(e.target.files);
+    files.forEach((file) => {
+      if (
+        file.type !== "application/pdf" &&
+        file.type !== "text/plain" &&
+        file.type !== "application/msword" &&
+        file.type !==
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document" &&
+        file.type !== "application/vnd.ms-powerpoint" &&
+        file.type !==
+          "application/vnd.openxmlformats-officedocument.presentationml.presentation" &&
+        file.type !== "application/vnd.rar" &&
+        file.type !== "application/zip" &&
+        file.type !== "application/x-7z-compressed" &&
+        file.type !== "audio/mpeg" &&
+        file.type !== "audio/wav" &&
+        file.type !== "image/png" &&
+        file.type !== "image/jpeg" &&
+        file.type !== "image/gif" &&
+        file.type !== "image/webp" &&
+        file.type !== "video/mp4" &&
+        file.type !== "video/mpeg" &&
+        file.type !== "video/webm"
+      ) {
+        files = files.filter((item) => item.name !== file.name);
+        return;
+      } else if (file.size > 1024 * 1024 * 10) {
+        files = files.filter((item) => item.name !== file.name);
+        return;
+      } else {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = (e) => {
+          dispatch(
+            addFiles({
+              file: file,
+              fileData:
+                getFileType(file.type) === "IMAGE" ? e.target.result : "",
+              type: getFileType(file.type),
+            })
+          );
+        };
+      }
+    });
+  };
+  return (
+    <>
+      <div
+        className="h-14 w-14 border mt-2 dark:border-white rounded-md flex items-center justify-center cursor-pointer"
+        onClick={() => inputRef.current.click()}
+      >
+        <span className="rotate-45">
+          <CloseIcon className="dark:fill-dark_svg_1" />
+        </span>
+      </div>
+      <input
+        type="file"
+        hidden
+        multiple
+        ref={inputRef}
+        accept="application/pdf, text/plain, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document, 
+        application/vnd.ms-powerpoint, application/vnd.openxmlformats-officedocument.presentationml.presentation, application/vnd.rar, application/zip, 
+        application/x-7z-compressed, audio/mpeg, audio/wav, image/png, image/jpeg, image/gif, image/webp, video/mp4, video/mpeg, video/webm"
+        onChange={filesHandler}
+      />
+    </>
+  );
 }
--- src/components/chat/preview/files/FileViewer.jsx
+++ src/components/chat/preview/files/FileViewer.jsx
@@ -4,9 +4,9 @@ export default function FileViewer({ activeIndex }) {
   const { files } = useSelector((state) => state.chat);
   return (
     <div className="w-full max-w-[60%]">
-      {/* Container */}
+      {/*Container */}
       <div className="flex justify-center items-center">
-        {files[0].type === "IMAGE" ? (
+        {files[activeIndex].type === "IMAGE" ? (
           <img
             src={files[activeIndex].fileData}
             alt=""
@@ -19,14 +19,16 @@ export default function FileViewer({ activeIndex }) {
               src={`../../../../images/file/${files[activeIndex].type}.png`}
               alt={files[activeIndex].type}
             />
-            {/* No preview text */}
-            <h1 className="dark: text-dark_text_2 text-2xl">
+            {/*No preview text*/}
+            <h1 className="dark:text-dark_text_2 text-2xl">
               No preview available
             </h1>
-            {/* File size /type */}
+            {/*File size / type*/}
             <span className="dark:text-dark_text_2">
-              {Math.round(files[activeIndex]?.file?.size / 1000)} kB -{" "}
-              {files[0].type}
+              {files[activeIndex]?.file?.size < 1000
+                ? "1"
+                : Math.round(files[activeIndex]?.file?.size)}{" "}
+              kB - {files[activeIndex]?.type}
             </span>
           </div>
         )}
